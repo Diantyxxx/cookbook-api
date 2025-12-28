0a9d65e77f7a65218d1a244aa45e200533e2de82
Diantyxxx <lansi@inbox.ru>
2025-12-28 18:43:59 +0300

    Create models.py

---
+++ models.py
@@ -0,0 +1,17 @@
+from sqlalchemy.orm import Mapped, mapped_column
+
+from database import Base
+
+
+class Recipe(Base):
+    __tablename__ = "recipes"
+
+    recipe_id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    title: Mapped[str] = mapped_column(index=True, nullable=False)
+    cooking_time: Mapped[int] = mapped_column(nullable=False)
+    ingredients: Mapped[str] = mapped_column(nullable=False)
+    description: Mapped[str] = mapped_column(nullable=False)
+    views: Mapped[int] = mapped_column(default=0)
+
+    def __repr__(self) -> str:
+        return f"Recipe(id={self.recipe_id}, title={self.title}, cooking time={self.cooking_time})"
