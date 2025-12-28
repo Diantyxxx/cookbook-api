8a242619350c1386129af16d8966aa82387f3cc7
Diantyxxx <lansi@inbox.ru>
2025-12-28 18:43:15 +0300

    Create database.py

---
+++ database.py
@@ -0,0 +1,22 @@
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
+
+DATABASE_URL = "sqlite+aiosqlite:///./cookbook.db"
+
+
+class Base(DeclarativeBase):
+    """Базовый класс для всех моделей"""
+
+    pass
+
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+async_session_maker = async_sessionmaker(
+    bind=engine, expire_on_commit=False, class_=AsyncSession
+)
+
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
